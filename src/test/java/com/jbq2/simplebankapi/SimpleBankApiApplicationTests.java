d92d08c8c7d68abad3cf39abb90372f2359b796b
Joshua Quizon <joshquizon@gmail.com>
2022-11-04 12:57:30 -0400

    initial commit

---
+++ src/test/java/com/jbq2/simplebankapi/SimpleBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbq2.simplebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
