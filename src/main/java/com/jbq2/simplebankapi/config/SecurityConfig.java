2b2a2ebe4eb0f5cee471d4b1090f450fa326ee21
Joshua Quizon <joshquizon@gmail.com>
2022-11-21 07:59:55 -0500

    security config no longer uses WebMvcConfigurerAdapter

--- src/main/java/com/jbq2/simplebankapi/config/SecurityConfig.java
+++ src/main/java/com/jbq2/simplebankapi/config/SecurityConfig.java
@@ -2,16 +2,18 @@ package com.jbq2.simplebankapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        return http.build();
     }
 }
