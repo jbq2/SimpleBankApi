5c99d164461504cee4d9015ed4e01bfb66952cdd
Joshua Quizon <joshquizon@gmail.com>
2022-11-22 16:47:24 -0500

    adding security constants

---
+++ src/main/java/com/jbq2/simplebankapi/constant/SecurityConstant.java
@@ -0,0 +1,15 @@
+package com.jbq2.simplebankapi.constant;
+
+public class SecurityConstant {
+    public static final long EXPIRATION_TIME = 3_600_000;
+    public static final String TOKEN_HEADER = "Bearer ";
+    public static final String JWT_TOKEN_HEADER = "Jwt-Token";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token cannot be verified";
+    public static final String BLUE_PIG_BANK = "Blue Pig Bank";
+    public static final String BLUE_PIG_BANK_ADMINISTRATION = "Blue Pig Bank Administrator";
+    public static final String AUTHORITIES = "Authorities";
+    public static final String FORBIDDEN_MESSAGE = "You need to login to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You do not have permission to access this page";
+    public static final String OPTIONS_HTTP_METHOD = "OPTIONS";
+    public static final String[] PUBLIC_URLS = {"/api/v1/login", "/api/v1/register"};
+}
