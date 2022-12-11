676a89fa0e49db21a26f879a12aa1757c4f9fab6
Joshua Quizon <joshquizon@gmail.com>
2022-12-10 23:12:35 -0500

    lots of changes, will be explained in comments

--- src/main/java/com/jbq2/simplebankapi/endpoints/public_accessible/login/LoginController.java
+++ src/main/java/com/jbq2/simplebankapi/endpoints/public_accessible/login/LoginController.java
@@ -1,18 +1,22 @@
 package com.jbq2.simplebankapi.endpoints.public_accessible.login;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jbq2.simplebankapi.session_management.SessionService;
-import com.jbq2.simplebankapi.user_packages.pojo.User;
 import com.jbq2.simplebankapi.user_packages.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 
 @RestController
 @AllArgsConstructor
@@ -26,20 +30,47 @@ public class LoginController {
     @ResponseBody
     public ResponseEntity<?> loginUser(@RequestBody LoginForm loginForm) {
         manager.authenticate(new UsernamePasswordAuthenticationToken(loginForm.getEmail(), loginForm.getPassword()));
-
         try{
+            /*
+            * upon successful login, register a session for the user
+            * get the user details to gather the authorities--put authorities in a list of string
+            * this will be returned if all goes well
+            * */
             final String sessionId = sessionService.registerSession(loginForm.getEmail());
             UserDetails userDetails = userService.loadUserByUsername(loginForm.getEmail());
+            Collection<? extends GrantedAuthority> grantedAuthoritiesCollection = userDetails.getAuthorities();
+            List<String> authorities = new ArrayList<>();
+            for(GrantedAuthority auth : grantedAuthoritiesCollection){
+                authorities.add(auth.getAuthority());
+            }
             return new ResponseEntity<>(
-                    new LoginDto(sessionId, userDetails.getAuthorities(), "Successfully logged in!"),
+                    new LoginResponse(sessionId, authorities, "Successfully logged in!"),
                     HttpStatus.OK
             );
         }
         catch(RuntimeException e){
+            /*
+            * runtime exception is thrown if there is a duplicate key violation when registering a session
+            * this happens when a user has already signed in
+            * */
             return new ResponseEntity<>(
                     "There is already an existing session.",
                     HttpStatus.INTERNAL_SERVER_ERROR
             );
+        }
+    }
+
+
+    @GetMapping("/verify")
+    public ResponseEntity<?> isLoggedIn(@RequestHeader String sessionId) throws JsonProcessingException {
+        System.out.println(sessionId);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            sessionService.touchSession(sessionId);
+            return new ResponseEntity<>(objectMapper.writeValueAsString(true), HttpStatus.OK);
+        }
+        catch(RuntimeException e){
+            return new ResponseEntity<>(objectMapper.writeValueAsString(false), HttpStatus.OK);
         }
     }
 }
--- src/main/java/com/jbq2/simplebankapi/endpoints/public_accessible/login/LoginResponse.java
+++ src/main/java/com/jbq2/simplebankapi/endpoints/public_accessible/login/LoginResponse.java
@@ -2,15 +2,15 @@ package com.jbq2.simplebankapi.endpoints.public_accessible.login;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class LoginDto {
+public class LoginResponse {
+    /*
+    * login response will contain the user's session id, authorities, and a developer message
+    * */
     private String session_id;
-    private Collection<? extends GrantedAuthority> authorities;
+    private List<String> authorities;
     private String message;
 }
