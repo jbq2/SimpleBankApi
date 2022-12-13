880f649d94144028c0c5c21d99c60c167c7bc270
Joshua Quizon <joshquizon@gmail.com>
2022-12-13 11:39:21 -0500

    creating profile controller

---
+++ src/main/java/com/jbq2/simplebankapi/endpoints/authenticated_accessible/user/ProfileController.java
@@ -0,0 +1,11 @@
+package com.jbq2.simplebankapi.endpoints.authenticated_accessible.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/user")
+public class ProfileController {
+}
