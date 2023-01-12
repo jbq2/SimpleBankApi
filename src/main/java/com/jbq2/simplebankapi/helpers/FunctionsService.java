733e05881956b925032de4bf6b009dbcacc9c75d
jbq2 <jbq2@njit.edu>
2023-01-12 16:49:50 -0500

    documnenting helpers and jwt packages

--- src/main/java/com/jbq2/simplebankapi/helpers/FunctionsService.java
+++ src/main/java/com/jbq2/simplebankapi/helpers/FunctionsService.java
@@ -4,16 +4,32 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.jbq2.simplebankapi.jwt.JwtConstants;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
+/**
+ * Service that provides methods to update and create JSON web tokens and checks for user login status.
+ * @author Joshua Quizon
+ * @version 0.1
+ */
 @Service
-@AllArgsConstructor
 public class FunctionsService {
-    private JwtConstants jwtConstants;
+    private final JwtConstants jwtConstants;
 
+    /**
+     * Initializes the JwtConstants attribute of the FunctionsService class.
+     * @param jwtConstants Provides static variables used for decoding JSON web tokens.
+     */
+    public FunctionsService(JwtConstants jwtConstants) {
+        this.jwtConstants = jwtConstants;
+    }
+
+    /**
+     * Updates an valid JSON web token.
+     * @param jwt The user's current JSON web token that is to be updated.
+     * @return Returns a new String that is the updated JSON web token.
+     */
     public String updateUserJwtExpiry(String jwt) {
         DecodedJWT decodedJwt = JWT.require(Algorithm.HMAC256(jwtConstants.key)).build().verify(jwt);
         String[] authorities = decodedJwt.getClaim("authorities").asArray(String.class);
@@ -24,6 +40,13 @@ public class FunctionsService {
                 .sign(Algorithm.HMAC256(jwtConstants.key));
     }
 
+    /**
+     * Creates a new JSON web token given the subject, an array of authorities, and an expiry time.
+     * @param subject The user's email address.
+     * @param authorities The authorities of the user as Strings.
+     * @param expiryTime The expiry time of the newly created JSON web token.
+     * @return Returns a String that is the newly created JSON web token.
+     */
     public String createUserJwt(String subject, String[] authorities, Date expiryTime) {
         return JWT.create()
                 .withSubject(subject)
@@ -32,15 +55,15 @@ public class FunctionsService {
                 .sign(Algorithm.HMAC256(jwtConstants.key));
     }
 
+    /**
+     * Checks if a user is logged in by verifying the user's JSON web token.
+     * @param jwt The JSON web token of the user.
+     * @return Returns true if the JSON web token is valid, and false otherwise.
+     */
     public boolean isLoggedIn(String jwt) {
         try{
             DecodedJWT decodedJwt = JWT.require(Algorithm.HMAC256(jwtConstants.key)).build().verify(jwt);
-            if(!decodedJwt.getExpiresAt().before(new Date())) {
-                return true;
-            }
-            else{
-                return false;
-            }
+            return !decodedJwt.getExpiresAt().before(new Date());
         }
         catch(RuntimeException e) {
             return false;
--- src/main/java/com/jbq2/simplebankapi/helpers/IsLoggedInResponse.java
+++ src/main/java/com/jbq2/simplebankapi/helpers/IsLoggedInResponse.java
@@ -3,9 +3,23 @@ package com.jbq2.simplebankapi.helpers;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Plain-old-Java-object that contains the details to be sent back to the web application upon checking if a user is logged in.
+ * @author Joshua Quizon
+ * @version 0.1
+ */
 @Data
-@AllArgsConstructor
 public class IsLoggedInResponse {
     private boolean loggedIn;
     private String updatedJwt;
+
+    /**
+     * Initializes the 2 attributes of the IsLoggedInResponse class.
+     * @param loggedIn True if the user is logged in, and false otherwise.
+     * @param updatedJwt The updated JSON web token of the user.
+     */
+    public IsLoggedInResponse(boolean loggedIn, String updatedJwt) {
+        this.loggedIn = loggedIn;
+        this.updatedJwt = updatedJwt;
+    }
 }
